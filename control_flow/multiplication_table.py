f865755ac80d3617a7f9273fe9afb8ec1b6028ca
Docia Asafo-Adjei <adocia30@gmail.com>
2025-11-16 12:31:47 -0800

    git commit -m "multiplication_table".py

---
+++ control_flow/multiplication_table.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number to see its multiplication "))
+5
+for i in range(1, 11):
+    print(f"{number} * {i} = {result}")
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
+
