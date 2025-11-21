32d632a835d1fbaf34ec32cc5b1c73b93f93636a
Docia Asafo-Adjei <adocia30@gmail.com>
2025-11-21 07:41:15 -0800

    git commit -m "shopping_list_manager".py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -4,21 +4,27 @@ def display_menu():
     print("2. Remove an item")
     print("3. View the list")
     print("4. Exit")
+
 def main():
     shopping_list = []
+
     while True:
         display_menu()
         choice = int(input("Enter your choice: "))
 
         if choice == '1':
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} has been added to the shopping list.")
+
         elif choice == '2':
+            item = ("Enter the item to remove: ")
             shopping_list.remove(item)
-            print(f"{tem} has been removed from the shopping list.")
-            pass
+            print(f"{item} has been removed from the shopping list.")
+            
         elif choice == '3':
-            print(shopping_list)
+            print("Current shopping list:", shopping_list)
+            
         elif choice == '4':
             print("Good bye!")
             break
