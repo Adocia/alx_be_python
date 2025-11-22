c92e930dd1a1662bfef0acbad0cd672d95badd13
Docia Asafo-Adjei <adocia30@gmail.com>
2025-11-21 22:19:57 -0800

    git commit -m "shopping_list_manager".py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,7 @@
 def display_menu():
     print("Shopping List Manager")
-    print("1. Add an item")
-    print("2. Remove an item")
+    print("1. Add item")
+    print("2. Remove item")
     print("3. View the list")
     print("4. Exit")
 
@@ -30,6 +30,6 @@ def main():
             break
         else:
             print("Invalid choice. Please try again")
-            
+
 if __name__ == "__main__":
   main()
