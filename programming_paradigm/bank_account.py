15be471078d68e84922b9e94b0e8e14c2d43daba
Docia Asafo-Adjei <adocia30@gmail.com>
2025-11-26 03:57:17 -0800

    git commit -m "bank_account".py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -24,7 +24,5 @@ class BankAccount:
             return True
 
     def display_balance(self):
-     """Print the current account balance with 2 decimal places."""
-    print(f"Current Balance: ${self.__account_balance:.2f}")
-
-    
+        """Print the current account balance."""
+        print(f"Current Balance: ${self.__account_balance}")
